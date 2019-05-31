ef4e0f1308f6bf7cc016e6d073fbac3affda790a
Belinda Lei <belinda.lei95@gmail.com>
2019-05-31 01:43:33 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+def meal_choice(meal="meat")
+
+authenticate_or_request_with_http_digest
